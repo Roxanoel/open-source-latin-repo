7a3e6f6d2f3b46127ed01e26f217bcc8e7afa333
Roxane Noël <roxane.noel92@gmail.com>
2022-10-24 12:53:46 +0100

    Added a temp title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,10 @@
 function Header() {
 
+    return (
+        <header>
+            An open repository of translations for Latin Medieval Philosophy texts
+        </header>
+    )
 }
 
 export default Header;
