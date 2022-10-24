0adc47e472348fe82f740c73ad9b1d7e76d81873
Roxane Noël <roxane.noel92@gmail.com>
2022-10-24 13:46:57 +0100

    Added blank Search component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,15 @@
-function Nav() {
+import Search from "../Search/Search";
 
+function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>Cat 1</li>
+                <li>Cat 2</li>
+            </ul>
+            <Search />
+        </nav>
+    );
 }
 
 export default Nav;
---
+++ src/components/Search/Search.js
@@ -0,0 +1,8 @@
+function Search() {
+    return (
+        <form>
+            <input type="text" id="search-input" />
+        </form>
+    )
+}
+export default Search;
